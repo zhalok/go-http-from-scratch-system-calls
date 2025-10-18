61a87dab937c4d7697675d990957cf30fbfc1246
zhalok <rahmanzhalok@gmail.com>
2025-10-18 17:48:04 +0600

    shutting down gracefully when we are exitng the process but the connections are still alive we may get error when we are trying to re run the process on the same ip and port that ip and port is already in use

--- main.go
+++ main.go
@@ -2,12 +2,66 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
 	"strings"
 	"syscall"
 )
 
+var activeConnections []int
+
+func closeConnectionWithLog(connectionFd int) {
+	fmt.Printf("Closing connection %d\n", connectionFd)
+	syscall.Close(connectionFd)
+}
+
+func acknowledgeClient(connectionFd int) {
+	_, err := syscall.Write(connectionFd, []byte("Connection Accepted\n"))
+	if err != nil {
+		fmt.Printf("Can not accept connection: %d\n", connectionFd)
+	}
+}
+
+func removeConnection(connectionFd int) {
+
+	closeConnectionWithLog(connectionFd)
+	newActiveConnection := make([]int, 0)
+
+	for _, activeConnection := range activeConnections {
+		if activeConnection != connectionFd {
+			newActiveConnection = append(newActiveConnection, activeConnection)
+		}
+
+	}
+	activeConnections = newActiveConnection
+
+}
+
+func cleanActiveConnections() {
+	for _, activeConnection := range activeConnections {
+		closeConnectionWithLog(activeConnection)
+	}
+}
+
+func handleInterrupts() {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("\nShutting down gracefully...")
+
+		cleanActiveConnections()
+
+		os.Exit(0)
+	}()
+
+
+	select {}
+}
+
 func readFromConnectionSocket(connectionFd int) {
-	defer syscall.Close(connectionFd)
+	defer removeConnection(connectionFd)
 
 	buf := make([]byte, 1024)
 	message := ""
@@ -16,13 +70,11 @@ func readFromConnectionSocket(connectionFd int) {
 		readBytes, err := syscall.Read(connectionFd, buf)
 		if err != nil {
 			fmt.Printf("Error reading from connection %d\n", connectionFd)
-			syscall.Close(connectionFd)
 			return
 		}
-		if readBytes == 0{
-			fmt.Printf("Client has closed connection: %d\n",connectionFd)
-			fmt.Printf("Closing connection %d\n",connectionFd)
-			syscall.Close(connectionFd)
+		if readBytes == 0 {
+			fmt.Printf("Client has closed connection: %d\n", connectionFd)
+			fmt.Printf("Closing connection %d\n", connectionFd)
 			return
 		}
 
@@ -32,7 +84,7 @@ func readFromConnectionSocket(connectionFd int) {
 			payaload = splited[0]
 			message = splited[1]
 		}
-		fmt.Printf("Recieved Message from connection %d: %s\n",connectionFd, payaload)
+		fmt.Printf("Recieved Message from connection %d: %s\n", connectionFd, payaload)
 
 	}
 }
@@ -71,6 +123,9 @@ func main() {
 	}
 
 	fmt.Println("Listening on 127.0.0.1:8080")
+	activeConnections = make([]int, 0)
+
+	go handleInterrupts()
 
 	for {
 		connectionFd, connectionAddress, err := syscall.Accept(listeningSocketFd)
@@ -78,6 +133,8 @@ func main() {
 			fmt.Printf("there was a problem accepting the connection from the listening socket %+v\n", err)
 			continue
 		}
+		activeConnections = append(activeConnections, connectionFd)
+		acknowledgeClient(connectionFd)
 		addressAndPort := connectionAddress.(*syscall.SockaddrInet4)
 		fmt.Printf("recieved connection from %s, connection fd: %d\n", extractAddressAndPort(*addressAndPort), connectionFd)
 		readFromConnectionSocket(connectionFd)
