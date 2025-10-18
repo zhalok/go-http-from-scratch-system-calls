31b113dd25d19adb247a319aceb752a98c558c67
zhalok <rahmanzhalok@gmail.com>
2025-10-19 02:12:16 +0600

    built http header and meta data parser from socket read

--- main.go
+++ main.go
@@ -4,22 +4,29 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"strings"
 	"syscall"
 )
 
+
 var activeConnections []int
+
+type MetaData struct{
+	path string
+	method string
+	queryMap map[string]string
+}
+
+type Request struct{
+	metadata MetaData
+	headers map[string]string
+	body string
+}
 
 func closeConnectionWithLog(connectionFd int) {
 	fmt.Printf("Closing connection %d\n", connectionFd)
 	syscall.Close(connectionFd)
-}
-
-func acknowledgeClient(connectionFd int) {
-	_, err := syscall.Write(connectionFd, []byte("Connection Accepted\n"))
-	if err != nil {
-		fmt.Printf("Can not accept connection: %d\n", connectionFd)
-	}
 }
 
 func removeConnection(connectionFd int) {
@@ -56,9 +63,77 @@ func handleInterrupts() {
 		os.Exit(0)
 	}()
 
-
 	select {}
 }
+
+func parseMetaData(urlString string) MetaData {
+	terms := strings.Split(urlString," ")
+	method := strings.TrimSpace(terms[0])
+	path := strings.TrimSpace(terms[1])
+	queryMap:= parseQueryString(path)
+
+	return MetaData{
+		method: method,
+		path: strings.TrimSpace(strings.Split(path,"?")[0]),
+		queryMap: queryMap,
+	}
+}
+
+func parseQueryString(pathString string) map[string]string{
+	pathAndQueryStrings := strings.Split(pathString, "?")
+
+	if len(pathAndQueryStrings) < 2{
+		return nil
+	}
+
+	queryString := pathAndQueryStrings[1]
+
+	keyValueMap := make(map[string]string)
+	
+	keyValues := strings.Split(queryString,"&")
+
+	for _, keyValue := range keyValues{
+		keyAndValue := strings.Split(keyValue,"=")
+		key := keyAndValue[0]
+		value := keyAndValue[1]
+		keyValueMap[key] = value
+	}
+
+	return keyValueMap
+}
+
+func parseHeaders(hearderStrings []string) map[string]string {
+	headersMap := make(map[string]string)
+    for _, headerString := range hearderStrings{
+
+		headerString = strings.TrimSpace(headerString)
+
+		if headerString == ""{
+			continue
+		}
+
+		keyValue := strings.Split(headerString,":")
+	
+		key := strings.TrimSpace(strings.ToLower( keyValue[0]))
+		value := strings.TrimSpace(strings.ToLower(keyValue[1]))
+		headersMap[key] = value
+	}
+
+	return headersMap
+}
+
+func parseHttpRequest(rawRequest string) Request {
+	lines := strings.Split(rawRequest,"\n")
+	metaData := parseMetaData(lines[0])
+
+	headersMap := parseHeaders(lines[1:])
+
+	return Request{
+		metadata: metaData,
+		headers: headersMap,
+	}
+
+} 
 
 func readFromConnectionSocket(connectionFd int) {
 	defer removeConnection(connectionFd)
@@ -77,14 +152,32 @@ func readFromConnectionSocket(connectionFd int) {
 			fmt.Printf("Closing connection %d\n", connectionFd)
 			return
 		}
-
 		message += string(buf[:readBytes])
-		if strings.Contains(message, "\n") {
-			splited := strings.Split(message, "\n")
-			payaload = splited[0]
-			message = splited[1]
+		lines := strings.Split(message, "\n")
+
+		for _, line := range lines[0 : len(lines)-1] {
+			if line != "" {
+				payaload += fmt.Sprintf("%s\n", line)
+			}
 		}
-		fmt.Printf("Recieved Message from connection %d: %s\n", connectionFd, payaload)
+
+		message = lines[len(lines)-1]
+		
+		request := parseHttpRequest(payaload)
+		headers := request.headers
+	
+		contentLength, err := strconv.Atoi(headers["content-length"])
+		if err != nil{
+			fmt.Printf("error while getting content length string %v\n",err)
+			panic(err)
+		}
+
+		fmt.Printf("Content length %d\n",contentLength)
+		if len(message) >= contentLength{
+			request.body = strings.TrimSpace(message)
+		}
+
+		fmt.Printf("request body %s\n",request.body)
 
 	}
 }
@@ -134,7 +227,6 @@ func main() {
 			continue
 		}
 		activeConnections = append(activeConnections, connectionFd)
-		acknowledgeClient(connectionFd)
 		addressAndPort := connectionAddress.(*syscall.SockaddrInet4)
 		fmt.Printf("recieved connection from %s, connection fd: %d\n", extractAddressAndPort(*addressAndPort), connectionFd)
 		go readFromConnectionSocket(connectionFd)
